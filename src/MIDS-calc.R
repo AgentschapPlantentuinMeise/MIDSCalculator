e9feb5cdcf156d20da66d757851b0ff5b0fbd142
LynnDelgat <lynn.delgat@plantentuinmeise.be>
2022-02-23 16:58:36 +0100

    Update to include criteria from read_json_criteria function

--- src/MIDS-calc.R
+++ src/MIDS-calc.R
@@ -9,39 +9,25 @@ gbif_dataset <- fread("data/occurrence.txt", encoding = "UTF-8")
 
 # Define criteria ---------------------------------------------------------
 
+# Get list of criteria
+list_criteria <- read_json_criteria()
+
 #level zero
 crits_mids_zero <- "!is.null(catalogNumber) & !is.null(institutionCode) & !is.null(modified)"
+#crits_mids_zero <- list_criteria$mids0 doesn't work yet because of subcond with xpath instead op property
 
 #level one
-#also needs materialtype and objecttype
-crits_mids_one <- "!is.null(scientificName)|!is.null(organismName)|!is.null(vernacularName) &
-                    !is.null(preparations)"
-
-#level two minimal
-#current: (country and locality,  or coordinates) + collector + collection date (year at the least)
+crits_mids_one <- list_criteria$mids1 #works
+  
+#level two
 crits_mids_two <- "(is.na(countryCode) & !is.na(locality)|
                   (!is.na(decimalLatitude)&!is.na(decimalLongitude)))&
                   (!is.na(recordedBy)|!is.na(recordedByID))&
-                  !is.na(eventDate)|!is.na(year)|!is.na(verbatimEventDate))"
-
-#level two strict
-#including most elements under consideration as required
-crits_mids_two_strict <- "!is.na(continent) &
-                          !is.na(countryCode) &
-                          !is.na(stateProvince) &
-                          !is.na(county) &
-                          !is.na(locality) &
-                          !is.na(decimalLatitude) &
-                          !is.na(decimalLongitude) &
-                          (!is.na(recordedBy)|!is.na(recordedByID))&
-                          !is.na(recordNumber) &
-                          !is.na(collectionID) &
-                          !is.na(typeStatus) &
-                          !is.na(eventDate)"
+                  !is.na(eventDate)|!is.na(year)|!is.na(verbatimEventDate)"
+#crits_mids_two <- list_criteria$mids2 doesn't work yet because of the empty property under deposited + verbatimCoordinates and country are not columns of the gbif dataset + error "x character string is not in a standard unambiguous format"
 
 #level three
-crits_mids_three <- "!is.na(na_if(mediaType, '')) & is.na(issue)" #mediatype contains empty strings
-
+crits_mids_three <- list_criteria$mids3 #works
 
 
 # Calculate MIDS level ----------------------------------------------------
@@ -54,22 +40,9 @@ gbif_dataset_mids <- gbif_dataset %>%
         !!rlang::parse_expr(crits_mids_zero) ~ 0
   ))
 
-gbif_dataset_mids <- gbif_dataset_mids %>%
-  mutate(mids_level_strict = case_when(
-    !!rlang::parse_expr(crits_mids_three) & !!rlang::parse_expr(crits_mids_two_strict) & !!rlang::parse_expr(crits_mids_one) & !!rlang::parse_expr(crits_mids_zero) ~ 3,
-    !!rlang::parse_expr(crits_mids_two_strict) & !!rlang::parse_expr(crits_mids_one) & !!rlang::parse_expr(crits_mids_zero) ~ 2,
-    !!rlang::parse_expr(crits_mids_one) & !!rlang::parse_expr(crits_mids_zero) ~ 1,
-    !!rlang::parse_expr(crits_mids_zero) ~ 0
-  ))
-
 # Summary -----------------------------------------------------------------
 
 gbif_dataset_mids %>%
   group_by(mids_level) %>%
-  tally()  %>%
-  mutate(perc = n / sum(n) *100)
-
-gbif_dataset_mids %>%
-  group_by(mids_level_strict) %>%
   tally()  %>%
   mutate(perc = n / sum(n) *100)
