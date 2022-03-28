b6d37886662091190762790454350a5a831c6fdb
LynnDelgat <lynn.delgat@plantentuinmeise.be>
2022-03-28 17:32:00 +0200

    Add plot and summary for MIDS criteria

--- src/Shiny_MIDS/MIDScalcApp.R
+++ src/Shiny_MIDS/MIDScalcApp.R
@@ -19,6 +19,7 @@ ui <- navbarPage("Calculate MIDS scores",
                  tabPanel("Results",
                           sidebarLayout(
                             sidebarPanel(
+                              #placeholders
                               helpText("Filter to calculate MIDS scores for part of the dataset"),
                               sliderInput("range", 
                                           label = "Range of interest:",
@@ -33,8 +34,10 @@ ui <- navbarPage("Calculate MIDS scores",
                             ),
                             mainPanel(
                               tabsetPanel(type = "tabs",
-                                          tabPanel("Plot", plotOutput("midsplot")),
-                                          tabPanel("Summary", DT::dataTableOutput("summary")),
+                                          tabPanel("Plot", plotOutput("midsplot"),
+                                                   plotOutput("midscritsplot")),
+                                          tabPanel("Summary", DT::dataTableOutput("summary"), 
+                                                   DT::dataTableOutput("summarycrit")),
                                           tabPanel("Table", 
                                                    DT::dataTableOutput("table"))
                               )
@@ -56,7 +59,14 @@ server <- function(input, output) {
   })
   #create summary of MIDS levels
   midssum <- reactive({
-    gbif_dataset_mids() %>% group_by(mids_level) %>% summarise(n = n(), percentage = n()/nrow(.)*100)
+    gbif_dataset_mids() %>% group_by(mids_level) %>% summarise(n = n(), percentage = round(n()/nrow(.)*100))
+  })
+  #create summary of MIDS criteria
+  midscrit <- reactive({
+    gbif_dataset_mids()[ , grep("mids[0-3]", names(gbif_dataset_mids())), with = FALSE] %>% 
+      map(~{round((sum(.x, na.rm = TRUE) / nrow(gbif_dataset_mids()))*100)}) %>%
+      as.data.table() %>%
+      t()
   })
   
   #plot mids levels
@@ -64,18 +74,31 @@ server <- function(input, output) {
     ggplot(midssum(), aes(x=mids_level, y=n)) + 
     geom_bar(stat = "identity", fill = rgb(0.1,0.4,0.5)) +
     coord_cartesian(xlim = c(-1.5, 3.5)) +
-    
-    geom_text(data = midssum(), aes(y = n + sum(n)*0.02, label = paste0(n, " (", round((n/sum(n))*100),"%)")))
+    geom_text(data = midssum(), 
+              aes(y = n + sum(n)*0.02, label = paste0(n, " (", round((n/sum(n))*100),"%)")))
+  })
+  #plot mids criteria
+  output$midscritsplot<-renderPlot({
+    ggplot(as.data.frame(midscrit()), aes(x= rownames(midscrit()), y=V1)) + 
+      geom_bar(stat = "identity", fill = rgb(0.1,0.4,0.5)) + 
+      coord_flip() + 
+      geom_text(data = as.data.frame(midscrit()), 
+                aes(y = V1, label = V1), hjust = 1.25, colour = "white")
   })
   
   #summary of mids levels
   output$summary <- DT::renderDataTable({
     midssum()
   })
-  #records table with mids levels
+  #summary of mids criteria
+  output$summarycrit <- DT::renderDataTable({
+    midscrit()
+  })
+  #records table with mids levels and criteria
   output$table <- DT::renderDataTable({
     gbif_dataset_mids()
   })
+  
   #download csv of record table with mids levels
   output$downloadData <- downloadHandler(
     filename = function() {
