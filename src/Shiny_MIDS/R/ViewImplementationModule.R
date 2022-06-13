eb87aa64c485e51a1beb16405decf49bdcf4aa6d
LynnDelgat <lynn.delgat@plantentuinmeise.be>
2022-06-13 13:35:58 +0200

    Improve code and layout of view MIDS implementation

--- src/Shiny_MIDS/R/ViewImplementationModule.R
+++ src/Shiny_MIDS/R/ViewImplementationModule.R
@@ -4,7 +4,41 @@ ViewImplementationUI <- function(id) {
   tagList(
     tabPanel(
       id,
-      actionButton(ns("view"), "Show MIDS implementation")
+      actionButton(ns("view"), "Show MIDS implementation"),
+      #css
+      tags$style(
+        HTML(
+         paste0('.', ns('title')), "{
+          text-align: center; 
+          background-color: #2874A6;
+          font-size: 25px; 
+          color: white;
+          box-shadow: 0px 5px 10px 0px rgba(0, 0, 0, 0.5)
+         }",
+         paste0('.', ns('midslevel')), "{
+          text-align: center;
+          background-color: #2874A6; 
+          color: white; 
+          font-size: 20px;
+          box-shadow: 0px 2.5px 5px 0px rgba(0, 0, 0, 0.5);
+         }",
+         paste0('.', ns('midselement')), "{
+          text-align: center; 
+          background-color: rgb(40, 116, 166, 0.2); 
+          font-size: 18px; 
+          color: #1A5276;
+          # border: 1px solid;
+          # border-color: #1A5276;
+          box-shadow: none;
+         }",
+         paste0('.', ns('grid')), "{
+           display: grid; 
+           grid-template-columns: 50% 50%; 
+           gap: 20px; 
+           padding: 20px;
+         }
+     ")
+      ),
     )
   )
 }
@@ -18,12 +52,11 @@ ViewImplementationServer <- function(id, schema, disable) {
       for (n_level in seq_along(schema()$criteria)){
         if (n_level == 1){
           #print title
-          print(HTML(paste0("<div style='text-align: center; background-color: #2874A6;
-              font-size: 25px; color: white'>", "MIDS criteria", "</div>")))
+          print(HTML(paste0("<div class=", ns('title'), "> MIDS criteria </div><br>")))
           print(HTML("<div style='text-align: center'> To reach a MIDS level, all of its elements must be met. </div>"))
           print(HTML("<div style='text-align: center'> To meet a MIDS element, one of its mappings must be present. </div>"))
           #initialize grid
-          print(HTML("<div style='display: grid; grid-template-columns: 50% 50%; gap: 20px; padding: 20px'>"))
+          print(HTML("<div class=", ns('grid'), ">"))
         }
         #determine position of element in grid
         if (n_level %% 2 == 0){
@@ -32,16 +65,16 @@ ViewImplementationServer <- function(id, schema, disable) {
         else {
           column <- 1
           row <- (n_level+1)/2}
-        print(HTML(paste0("<div style='background-color: #E5E7E9; grid-column: ",
-                          column, "; grid-row:", row, "'>")))
+        print(HTML(paste0("<div style='background-color: #EEF0F2; box-shadow: 0px 2.5px 5px 0px rgba(0, 0, 0, 0.5); 
+                          grid-column: ", column, "; grid-row:", row, "'>")))
         #MIDS levels
-        print(HTML("<div style='text-align: center;background-color: #2874A6; color: white; font-size: 20px'"))
+        print(HTML("<div class=", ns('midslevel')))
         print(h3(gsub("mids", "Level ", names(schema()$criteria)[[n_level]])))
         print(HTML("</div>"))
         #MIDS elements
         mids_el <- schema()$criteria[[n_level]]
         for(n_element in seq_along(mids_el)){
-          print(HTML("<div style='text-align: center; background-color: rgb(40, 116, 166, 0.2); font-size: 18px; color: #1A5276'"))
+          print(HTML("<div class=", ns('midselement'))) 
           print(h4(names(mids_el)[[n_element]]))
           print(HTML("</div>"))
           #MIDS mappings
@@ -73,11 +106,10 @@ ViewImplementationServer <- function(id, schema, disable) {
       values <- schema()$UoM[[n_prop]]
       if (n_prop == 1)
       {#print title
-        print(HTML(paste0("<div style='text-align: center; background-color: rgb(40, 116, 166, 0.8); 
-              font-size: 25px; color: white'>",
-                          "Unknown or missing values", "</div>")))
+        print(HTML(paste0("<div class=", ns('title'), "> Unknown or missing values </div><br>")))
+        print(HTML("<div style='text-align: center'> When a property has a value specified here, it will be regarded as absent/not digitized. </div>"))
         #set up grid layout
-        print(HTML("<div style='display: grid; grid-template-columns: 50% 50%; gap: 20px; padding: 20px'>"))}
+        print(HTML("<div class=", ns('grid'), ">"))}
       #determine position of element in grid
       if (n_prop %% 2 == 0){
         column <- 2
@@ -85,11 +117,11 @@ ViewImplementationServer <- function(id, schema, disable) {
       else {
         column <- 1
         row <- (n_prop+1)/2}
-      print(HTML(paste0("<div style='background-color: #E5E7E9; grid-column: ",
-                        column, "; grid-row:", row, "'>")))
+      print(HTML(paste0("<div style='background-color: #EEF0F2; box-shadow: 0px 2.5px 5px 0px rgba(0, 0, 0, 0.5);
+                        grid-column: ", column, "; grid-row:", row, "'>")))
       #Property
-      print(HTML("<div style='text-align: center;background-color: rgb(40, 116, 166, 0.8); color: white; font-size: 20px'"))
-      print(h3(prop))
+      print(HTML("<div class=", ns('midslevel')))
+      print(h3(stringr::str_to_title(prop)))
       print(HTML("</div>"))
       #Unknown or missing values for that property
       for (value in values){
