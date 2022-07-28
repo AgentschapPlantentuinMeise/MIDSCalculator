a047ebc66bea10fae31a205d604c0bb928b9e590
Mathias Dillen <37537540+matdillen@users.noreply.github.com>
2022-07-28 12:04:55 +0200

    Refine installer script
    Fixing bugged rinno functions now scripted
    Fix bug with default icons not being removed in time
    Stop overwriting info files

--- src/rinno_installer.R
+++ src/rinno_installer.R
@@ -2,33 +2,60 @@ library(RInno)
 
 source("src/packages.R")
 
-trace(RInno::code_section,edit=T) #replace 1-3 with 1-4
-trace(get_R,edit=T) #replace 1-3 with 1-4
+#fix bad R version numbers in two functions
+#loads updated functions in current environment only
 
+body(code_section)[[4]][[3]][[2]][[2]][[2]] <- substitute(
+  stringr::str_extract(readLines("https://cran.rstudio.com/bin/windows/base/",
+                                 warn = F), "[1-4]\\.[0-9]+\\.[0-9]+")
+  )
+body(code_section)[[4]][[3]][[3]][[2]] <- substitute(
+  stringr::str_extract(readLines("https://cran.rstudio.com/bin/windows/base/old/", 
+                                 warn = F), "[1-4]\\.[0-9]+\\.[0-9]+")
+)
+body(get_R)[[4]][[3]][[2]][[2]][[3]] <- substitute(
+  stringr::str_extract("[1-4]\\.[0-9]+\\.[0-9]+")
+)
+body(get_R)[[5]][[3]][[2]][[3]] <- substitute(
+  stringr::str_extract("[1-4]\\.[0-9]+\\.[0-9]+")
+)
+
+#set the app name
 name = "MIDSCalculator"
 
-copy_installation()
+#copy installer default and required files to app's directory
+copy_installation(overwrite=F)
+file.remove("default.ico")
+file.remove("setup.ico")
+
+#download R installer if not already present in app's directory
 get_R()
+
+#create a BAT file that will launch the shiny app
 create_bat(app_name = name,
            app_dir = getwd())
+
+#create a config file for the app
 create_config(app_name = name,
               pkgs = packages,
               pkgs_path = "bin")
+
+#create an ISS file that declares how the installer should be built
 start_iss(app_name = name) %>%
   
   # C-like directives
   directives_section(include_R = T,
                      app_version = "0.3",
-                     publisher   = "Meise Botanic Garden", 
-                     main_url    = "https://github.com/AgentschapPlantentuinMeise/MIDSCalculator") %>%
+                     publisher = "Meise Botanic Garden", 
+                     main_url = "https://github.com/AgentschapPlantentuinMeise/MIDSCalculator") %>%
   
   # Setup Section
   setup_section(app_dir = getwd(),
                 setup_icon  = "src/Shiny_MIDS/www/icon2.ico",
                 dir_out = "RInno_installer",
-                pub_url     = "https://www.plantentuinmeise.be", 
-                sup_url     = "https://github.com/AgentschapPlantentuinMeise/MIDSCalculator/issues",
-                upd_url     = "https://github.com/AgentschapPlantentuinMeise/MIDSCalculator",
+                pub_url = "https://www.plantentuinmeise.be", 
+                sup_url = "https://github.com/AgentschapPlantentuinMeise/MIDSCalculator/issues",
+                upd_url = "https://github.com/AgentschapPlantentuinMeise/MIDSCalculator",
                 privilege = "lowest",
                 info_before = "infobefore.txt",
                 info_after = "infoafter.txt") %>%
@@ -44,11 +71,11 @@ start_iss(app_name = name) %>%
                 user_browser = "chrome") %>%
   
   # Icons Section
-  icons_section(app_desc       = paste0("The MIDSCalculator app allows users to calculate MIDS levels for",
-                                        " datasets of natural history specimens. Currently, only Darwin Core",
-                                        " Archives as produced by GBIF are supported."),
+  icons_section(app_desc = paste0("The MIDSCalculator app allows users to calculate MIDS levels for",
+                                  " datasets of natural history specimens. Currently, only Darwin Core",
+                                  " Archives as produced by GBIF are supported."),
                 app_dir = getwd(),
-                app_icon       = "src/Shiny_MIDS/www/icon2.ico",
+                app_icon = "src/Shiny_MIDS/www/icon2.ico",
                 prog_menu_icon = T,
                 desktop_icon   = T) %>%
   gsub("commonprograms","autoprograms",.) %>%
@@ -62,4 +89,6 @@ start_iss(app_name = name) %>%
   
   # Write the Inno Setup script
   writeLines(paste0(name,".iss"))
+
+#compile the installer based on the ISS file
 compile_iss()
